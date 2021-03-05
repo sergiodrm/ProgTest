1f0c21ab21fa02ac162a11a3d91259335e3bb743
sergiodrm <47793261+sergiodrm@users.noreply.github.com>
2021-03-05 20:23:59 +0100

    Initial project

---
+++ GitTest/Source/GitTest/GitTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GitTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITTEST_API AGitTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
